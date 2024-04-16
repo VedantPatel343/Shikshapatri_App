616a0879facb437b0eb4dfc87c93c0f1bc02ab53
Vedant Patel <vedantpatel343@gmail.com>
2024-04-16 16:46:26 +0530

    changed the size of image in ImageDialog

--- app/src/main/java/com/swaminarayan/shikshapatriApp/presentation/components/ImageDialog.kt
+++ app/src/main/java/com/swaminarayan/shikshapatriApp/presentation/components/ImageDialog.kt
@@ -3,8 +3,8 @@ package com.swaminarayan.shikshapatriApp.presentation.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -55,7 +55,8 @@ fun ImageDialog(
                     painter = painterResource(id = image),
                     contentDescription = null,
                     modifier = Modifier
-                        .size(400.dp)
+                        .fillMaxWidth(0.98f)
+                        .fillMaxHeight(0.75f)
                         .clip(CircleShape),
                     contentScale = ContentScale.Crop
                 )
